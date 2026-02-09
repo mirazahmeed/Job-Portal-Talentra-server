a84f23cf8c6589882a2738c4411b5700c92d3a0c
mirazahmeed <mirazahmed0619@gmail.com>
2026-02-10 03:27:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.7cdmalj.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+
+		const jobsCollection = client.db("talentra").collection("jobs");
+		//jobs api
+
+		app.get("/jobs", async (req, res) => {
+			const query = {};
+			const cursor = jobsCollection.find(query);
+			const jobs = await cursor.toArray();
+			res.send(jobs);
+		});
+
+		app.get("/jobs/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const job = await jobsCollection.findOne(query);
+			res.send(job);
+		});
+
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!",
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+	res.send("Job Portal Talentra server!");
+});
+
+app.listen(port, () => {
+	console.log(`Job Portal app Talentra server is running on port ${port}`);
+});
